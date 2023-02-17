e8eb161b335c2dcf070b50ca8c2a0d047a7b8f57
albertsimon <albertsimon1997@gmail.com>
2023-02-17 18:09:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Cart from "./components/Cart/Cart";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Pages from "./components/Pages/Pages";
+import Data from "./components/FlashDeals/Data";
 
 function App() {
+  const { productItems } = Data;
+
+  const [CartItem, setCartItem] = useState([]);
+  const addToCart = (product) => {
+    const productExit = CartItem.find((item) => item.id === product.id);
+
+    if (productExit) {
+      setCartItem(
+        CartItem.map((item) =>
+          item.id === product.id
+            ? { ...productExit, qty: productExit.qty + 1 }
+            : item
+        )
+      );
+    } else {
+      setCartItem([...CartItem, { ...product, qty: 1 }]);
+    }
+  };
+  const decreaseQty = (product) => {
+    const productExit = CartItem.find((item) => item.id === product.id);
+    if (productExit.qty === 1) {
+      setCartItem(CartItem.filter((item) => item.id !== product.id));
+    } else {
+      setCartItem(
+        CartItem.map((item) =>
+          item.id === product.id
+            ? { ...productExit, qty: productExit.qty - 1 }
+            : item
+        )
+      );
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header CartItem={CartItem} />
+        <Switch>
+          <Route path="/" exact>
+            <Pages productItems={productItems} addToCart={addToCart} s />
+          </Route>
+          <Route path="/cart" exact>
+            <Cart
+              CartItem={CartItem}
+              addToCart={addToCart}
+              decreaseQty={decreaseQty}
+            />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
